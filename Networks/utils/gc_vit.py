7a1a4e3cf32c90e87f0f4621094671da1fb222f9
c-bousmar <cyril.bousmar@gmail.com>
2024-11-24 17:48:53 +0100

    fix timm alert message

--- Networks/utils/gc_vit.py
+++ Networks/utils/gc_vit.py
@@ -22,7 +22,7 @@ def gc_vit_tiny(dimensions, heads, in_channels, pretrained=True):
 
 import torch
 import torch.nn as nn
-from timm.models.layers import trunc_normal_, DropPath, to_2tuple
+from timm.layers import trunc_normal_, DropPath, to_2tuple
 from timm.models._registry import register_model
 from timm.models._builder import build_model_with_cfg
 
